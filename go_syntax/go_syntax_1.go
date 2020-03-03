b2b867979df23230051d43499c960b046862ac23
Subham Rai <srai@localhost.localdomain>
2020-03-03 13:06:44 +0530

    adding go syntax code

---
+++ go_syntax/go_syntax_1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt" 
+
+func main(){
+	fmt.Println("My first go code")
+	
+	var age int = 21
+	var name string = "subham"
+	a := 4
+	b := 5
+	fmt.Println(name,age)
+	fmt.Println("sum of 2 num",a+b)
+}
